ec5efeb01ca4211cdbbf4a977c4c00adc7157b0b
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-01-13 22:11:12 +0300

    scss design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
+import "./Styles.scss"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="container">
+      <div className="generator">
+        <h2 className="generator_header">Password Generator</h2>
+        <div className="generator_password">
+          {/* Password */}
+          <button>Clipboard</button>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password-length">Password Length</label>
+          <input
+            type="number"
+            name="password-length"
+            id="password-length"
+            max="20"
+            min="7"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="uppercase-letters">Include Uppercase Letters</label>
+          <input
+            type="checkbox"
+            name="uppercase-leters"
+            id="uppercase-letters"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="include-numbers">Include Lower Case Letters</label>
+          <input
+            id="include-numbers"
+            className="include-numbers"
+            type="checkbox"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="include-symbols">Include Lower Case Letters</label>
+          <input
+            id="include-symbols"
+            className="include-symbols"
+            type="checkbox"
+          />
+        </div>
+      </div>
     </div>
   );
 }
